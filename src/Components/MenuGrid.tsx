d6eacf6bd3abdf32643ca95a3f7d786313dd2dba
Rafael Castro <ralphc1992@live.com>
2024-03-03 11:05:46 -0600

    adding a gallery

--- src/Components/MenuGrid.tsx
+++ src/Components/MenuGrid.tsx
@@ -1,23 +1,16 @@
-import React from "react";
 import blueDonut from "../imgs/blue-donut.jpg";
-import breakfast from "../imgs/breakfast.jpg";
+
 import brownSugar from "../imgs/brown-sugar-cinnamon.jpg";
 import caramelFrap from "../imgs/caramel-frap.jpg";
-import chocDonut from "../imgs/choc-donut.jpg";
-import chocDonuts from "../imgs/choc-donuts.jpg";
+
 import chocGlazed from "../imgs/choc-glazed.jpg";
-import closingShop from "../imgs/closing-shop.jpg";
+
 import freshGlazed from "../imgs/fresh-glazed.jpg";
-import hotChocs from "../imgs/hot-chocolates.jpg";
-import insideShop from "../imgs/inside-shop.jpg";
-import inventory from "../imgs/inventory.jpg";
-import islandDonut from "../imgs/island-donut.jpg";
-import jellyFilled from "../imgs/jelly-filled.jpg";
+
 import mixedBox from "../imgs/mixed-box.jpg";
-import niceView from "../imgs/nice-view.jpg";
+
 import pinkDonuts from "../imgs/pink-donuts.jpg";
-import saltedCaramel from "../imgs/salted-caramel.jpg";
-import shopWindow from "../imgs/shop-window.jpg";
+
 import snickersFrap from "../imgs/snickers-frap.jpg";
 
 const MenuGrid = () => {
