0e1073b831c7fc33746f1329201aa99f67a5f671
Rafael Castro <ralphc1992@live.com>
2024-03-04 17:44:02 -0600

    reduced fonts

--- src/Components/MenuTabs.tsx
+++ src/Components/MenuTabs.tsx
@@ -54,57 +54,57 @@ const MenuTabs = () => {
       <div className="relative mx-auto w-[90%] h-[320px] p-10 max-w-[1200px] text-left md:text-center">
         {activeTab === "glazed" && (
           <>
-            <h1 className="text-white text-3xl">
+            <h1 className="text-white text-2xl">
               Glazed Donuts <span className="ml-1 text-hotpink">1.49</span>
             </h1>
-            <h1 className="text-white text-3xl">
+            <h1 className="text-white text-2xl">
               Chocolate Covered<span className="ml-1 text-hotpink">1.49</span>
             </h1>
-            <h1 className="text-white text-3xl">
+            <h1 className="text-white text-2xl">
               Caramel Covered<span className="ml-1 text-hotpink">1.49</span>
             </h1>
-            <h1 className="text-white text-3xl">
+            <h1 className="text-white text-2xl">
               Strawberry Covered<span className="ml-1 text-hotpink">1.49</span>
             </h1>
-            <h1 className="text-white text-3xl">
+            <h1 className="text-white text-2xl">
               Sprinkled Donuts<span className="ml-1 text-hotpink">1.79</span>
             </h1>
-            <h1 className="text-white text-3xl">
+            <h1 className="text-white text-2xl">
               Salted Caramel<span className="ml-1 text-hotpink">1.79</span>
             </h1>
-            <h1 className="text-white text-3xl">
+            <h1 className="text-white text-2xl">
               Maple Bacon<span className="ml-1 text-hotpink">1.79</span>
             </h1>
           </>
         )}
         {activeTab === "filled" && (
           <>
-            <h1 className="text-white text-3xl">
+            <h1 className="text-white text-2xl">
               White Cream Filled<span className="ml-1 text-hotpink">1.79</span>
             </h1>
-            <h1 className="text-white text-3xl">
+            <h1 className="text-white text-2xl">
               White Cream Eclair<span className="ml-1 text-hotpink">1.79</span>
             </h1>
-            <h1 className="text-white text-3xl">
+            <h1 className="text-white text-2xl">
               Bavarian<span className="ml-1 text-hotpink">1.79</span>
             </h1>
-            <h1 className="text-white text-3xl">
+            <h1 className="text-white text-2xl">
               Eclair<span className="ml-1 text-hotpink">1.79</span>
             </h1>
           </>
         )}
         {activeTab === "specialty" && (
           <>
-            <h1 className="text-white text-3xl">
+            <h1 className="text-white text-2xl">
               Cinnamon Twists<span className="ml-1 text-hotpink">2.99</span>
             </h1>
-            <h1 className="text-white text-3xl">
+            <h1 className="text-white text-2xl">
               Cinnamon Rolls<span className="ml-1 text-hotpink">2.99</span>
             </h1>
-            <h1 className="text-white text-3xl">
+            <h1 className="text-white text-2xl">
               Apple Fritters<span className="ml-1 text-hotpink">2.99</span>
             </h1>
-            <h1 className="text-white text-3xl">
+            <h1 className="text-white text-2xl">
               Donut Holes Dz<span className="ml-1 text-hotpink">2.99</span>
             </h1>
           </>
@@ -115,23 +115,23 @@ const MenuTabs = () => {
               <span className="text-hotpink">M</span>
               <span className="text-tan">L</span>
             </h1>
-            <h1 className="text-white text-3xl">
+            <h1 className="text-white text-2xl">
               Glazed Dozen<span className="ml-1 text-hotpink">7.49</span>
               <span className="ml-1 text-tan">12.99</span>
             </h1>
-            <h1 className="text-white text-3xl">
+            <h1 className="text-white text-2xl">
               Cake and Covered<span className="ml-1 text-hotpink">7.49</span>
               <span className="ml-1 text-tan">12.99</span>
             </h1>
-            <h1 className="text-white text-3xl">
+            <h1 className="text-white text-2xl">
               Maple Bacon<span className="ml-1 text-hotpink">9.49</span>
               <span className="ml-1 text-tan">17.99</span>
             </h1>
-            <h1 className="text-white text-3xl">
+            <h1 className="text-white text-2xl">
               Filled & Sprinkles<span className="ml-1 text-hotpink">9.49</span>
               <span className="ml-1 text-tan">17.99</span>
             </h1>
-            <h1 className="text-white text-3xl">
+            <h1 className="text-white text-2xl">
               Mixed Donuts<span className="ml-1 text-hotpink">9.49</span>
               <span className="ml-1 text-tan">17.99</span>
             </h1>
@@ -140,19 +140,19 @@ const MenuTabs = () => {
 
         {activeTab === "jelly" && (
           <>
-            <h1 className="text-white text-3xl">
+            <h1 className="text-white text-2xl">
               Blueberry Filled<span className="ml-1 text-hotpink">1.79</span>
             </h1>
-            <h1 className="text-white text-3xl">
+            <h1 className="text-white text-2xl">
               Apple Pie Filled<span className="ml-1 text-hotpink">1.79</span>
             </h1>
-            <h1 className="text-white text-3xl">
+            <h1 className="text-white text-2xl">
               Lemon Filled<span className="ml-1 text-hotpink">1.79</span>
             </h1>
-            <h1 className="text-white text-3xl">
+            <h1 className="text-white text-2xl">
               Raspberry Filled<span className="ml-1 text-hotpink">1.79</span>
             </h1>
-            <h1 className="text-white text-3xl">
+            <h1 className="text-white text-2xl">
               Strawberry Filled<span className="ml-1 text-hotpink">1.79</span>
             </h1>
           </>
@@ -160,19 +160,19 @@ const MenuTabs = () => {
 
         {activeTab === "cakes" && (
           <>
-            <h1 className="text-white text-3xl">
+            <h1 className="text-white text-2xl">
               Dutch Chocolate<span className="ml-1 text-hotpink">1.49</span>
             </h1>
-            <h1 className="text-white text-3xl">
+            <h1 className="text-white text-2xl">
               Sour Cream<span className="ml-1 text-hotpink">1.49</span>
             </h1>
-            <h1 className="text-white text-3xl">
+            <h1 className="text-white text-2xl">
               Blueberry<span className="ml-1 text-hotpink">1.49</span>
             </h1>
-            <h1 className="text-white text-3xl">
+            <h1 className="text-white text-2xl">
               Red Velvet<span className="ml-1 text-hotpink">1.49</span>
             </h1>
-            <h1 className="text-white text-3xl">
+            <h1 className="text-white text-2xl">
               Triple Dutch<span className="ml-1 text-hotpink">1.49</span>
             </h1>
           </>
